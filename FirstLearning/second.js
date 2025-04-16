84b788b6e35b5b751ecc3d5e65faeab2169c6138
chhaya03 <0188chhayamishra@gmail.com>
2025-04-16 10:56:21 +0530

    comment

---
+++ FirstLearning/second.js
@@ -0,0 +1,9 @@
+// in this file , i want to access second js data in firstcode.js
+
+chhaya ={
+  name:"Chhaya",
+  favNum :20,
+  developer:true
+}
+
+module.exports = chhaya;
---
+++ Module-wrapper-function/second.js
@@ -0,0 +1,14 @@
+// in this file , i want to access second js data in firstcode.js
+
+(function(exports,require,module,__filename,__dirname){
+
+    chhaya ={
+      name:"Chhaya",
+      favNum :20,
+      developer:true
+    }
+    
+    module.exports = chhaya;
+  
+  })
+
